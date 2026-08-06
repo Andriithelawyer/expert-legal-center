3bdaaef1877a539f7a56557cde52f0d6c337f481
Andrii Popov <a.s.popov.1@gmail.com>
2026-08-06 09:41:21 +0300

    Finalize footer contacts

--- app/data/contacts.ts
+++ app/data/contacts.ts
@@ -1,21 +1,28 @@
-const contacts = {
-  phone: "+38 (0XX) XXX-XX-XX",
+export const contacts = {
+  company: "Expert Legal Center",
 
-  phoneLink: "tel:+380000000000",
+  phone: "+380639397473",
+  phoneDisplay: "+380 63 939 74 73",
 
   email: "expertlegalcenter@ukr.net",
 
-  emailLink: "mailto:expertlegalcenter@ukr.net",
+  address: [
+    "вулиця Ростиславська, 11Б",
+    "Київ, 04116",
+    "офіс 309–312",
+  ],
+
+  workingHours: [
+    "Пн–Пт",
+    "09:00–18:00",
+  ],
 
   messengers: [
     "WhatsApp",
     "Telegram",
     "Viber",
-    "для телефонних дзвінків",
   ],
 
-  workingHours:
-    "Заявки через сайт приймаються цілодобово. Відповідаємо у найкоротший можливий час.",
+  notice:
+    "Звернення через форму на сайті приймаються цілодобово, у тому числі у вихідні та святкові дні. Ми опрацьовуємо кожне звернення у найкоротший можливий строк.",
 };
-
-export default contacts;
